5b63874bf353d72cae4d37c1a884521bc58badb7
Vignesh Kumar <xxvignesh@gmail.com>
2020-10-17 04:42:14 +0530

    add fits(), format file

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -9,8 +9,12 @@ impl Rectangle {
         self.width * self.height
     }
 
-    fn is_bigger(&self, other : &Rectangle) -> bool {
+    fn is_bigger(&self, other: &Rectangle) -> bool {
         self.area() > other.area()
+    }
+
+    fn fits(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
@@ -20,19 +24,26 @@ fn main() {
         height: 20,
     };
 
-    println!("The area is {} for rectangle with values: {:?}", a.area(), a);
+    println!(
+        "The area is {} for rectangle with values: {:?}",
+        a.area(),
+        a
+    );
 
     let b = Rectangle {
         width: 5,
-        height: 5
+        height: 5,
     };
 
     match a.is_bigger(&b) {
+        true => println!("Rectangle a is bigger than rectangle b."),
+        false => println!("Rectangle a is NOT bigger than rectangle b."),
+    }
+
+    match b.fits(&a) {
         true => {
-            println!("Rectangle a is bigger than rectangle b.")
-        },
-        false => {
-            println!("Rectangle a is NOT bigger than rectangle b.")
+            println!("Rectangle b fits rectangle a inside it.");
         }
+        false => println!("Rectangle b does NOT fit rectangle a inside it."),
     }
 }
